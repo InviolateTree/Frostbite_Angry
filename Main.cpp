338db5ee9b7788bae8b825301f8e85869b14cc1c
InviolateTree <Dean.cartwright88@live.co.uk>
2016-01-13 15:01:16 +0000

    Update Main.cpp
    Some star trek added

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,21 @@
 /*System Headers*/
 #include<iostream>
+#include<string>
 /*Custom Headers*/
 /*Main Program*/
 int mian()
 {
-/*Namespace*/
+	/*Namespace*/
 	namespace std;
+	/*User Input proerties*/
+	string User_Input;
 	/*statements*/
 	cout<<"Hello World!"<<endl<<"What can I do for you today? ";
+	cin>>User_Input>>endl;
+	if(User_Input="Clean my room")
+	{
+		cout<<"God Damit Jim I'm a computer not a maid!"<<endl;
+	}
 	system("pause");
 	return 0;
 }
