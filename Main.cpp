56475ec64a4f9a7af3076088120a52295e2c6aaf
InviolateTree <Dean.cartwright88@live.co.uk>
2016-01-13 10:58:47 +0000

    Create Main.cpp
    Starting File

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+/*System Headers*/
+#include<iostream>
+/*Custom Headers*/
+/*Main Program*/
+int mian()
+{
+/*Namespace*/
+	namespace std;
+	/*statements*/
+	cout<<"Hello World!"<<endl<<"What can I do for you today? ";
+	system("pause");
+	return 0;
+}
